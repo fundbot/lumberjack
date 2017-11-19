6fef168959804b3607981e5ed5e44d4f0ccfc607
Sudhanshu Raheja <sudhanshu@leftshift.io>
2017-11-19 18:27:30 +0700

    Looping in every second

--- main.go
+++ main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"time"
 )
 
 func main() {
+	go getFiles()
 
-	url := "http://example.qutheory.io/json"
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
 
-	body, err := download(url)
-	if err != nil {
-		fmt.Println(err)
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Pong %s!", r.URL.Path[1:])
+}
+
+func getFiles() {
+	for {
+		<-time.After(1 * time.Second)
+
+		url := "http://example.qutheory.io/json"
+
+		body, err := download(url)
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Println(body)
 	}
-
-	fmt.Println(body)
-
 }
