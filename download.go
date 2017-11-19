3886205be7698a2dbc8f8cc65314449063bcc628
Sudhanshu Raheja <sudhanshu@leftshift.io>
2017-11-19 18:08:08 +0700

    moved from a separate module to the same package

--- download.go
+++ download.go
@@ -1,4 +1,4 @@
-package downloader
+package main
 
 import (
 	"io/ioutil"
@@ -6,7 +6,7 @@ import (
 )
 
 // Download : pass a URL and get a string of it's output
-func Download(url string) (string, error) {
+func download(url string) (string, error) {
 	resp, err := http.Get(url)
 	defer resp.Body.Close()
 	if err != nil {
--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/fundbot/lumberjack/downloader"
 )
 
 func main() {
 
-	body, err := downloader.Download("http://example.qutheory.io/json")
+	body, err := download("http://example.qutheory.io/json")
 	if err != nil {
 		fmt.Println(err)
 	}
